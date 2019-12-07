33974d0cb13ede4ea5a525b13ebee6c3a76dfccd
condraft <condraft64@gmail.com>
2019-12-07 20:03:33 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,10 @@ class ApplicationController < ActionController::Base
   def production?
     Rails.env.production?
   end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV["BASIC_AUTH_USER2"] && password == ENV["BASIC_AUTH_PASSWORD2"]
     end
   end
 end
